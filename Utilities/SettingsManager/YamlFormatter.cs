b1821fea3f3a1e092c751faa4293d60aa1ead8db
Nikita Grishin <nikitagrishin1993@yandex.ru>
2019-10-25 23:09:47 +0300

    #2

--- Utilities/SettingsManager/YamlFormatter.cs
+++ Utilities/SettingsManager/YamlFormatter.cs
@@ -3,13 +3,26 @@ namespace SpaceEngineers.Core.Utilities.SettingsManager
     using CompositionRoot.Attributes;
     using CompositionRoot.Enumerations;
     using YamlDotNet.Serialization;
+    using YamlDotNet.Serialization.NamingConventions;
+    using YamlDotNet.Serialization.TypeResolvers;
 
     [Lifestyle(EnLifestyle.Singleton)]
     internal class YamlFormatter : FileSystemFormatterBase, IYamlFormatter
     {
-        private static readonly ISerializer _serializer = new SerializerBuilder().Build();
-
-        private static readonly IDeserializer _deserializer = new DeserializerBuilder().Build();
+        private static readonly ISerializer _serializer =
+            new SerializerBuilder()
+               .WithNamingConvention(PascalCaseNamingConvention.Instance)
+               .WithTypeResolver(new DynamicTypeResolver())
+               .WithMaximumRecursion(42)
+               .ConfigureDefaultValuesHandling(DefaultValuesHandling.Preserve)
+               .DisableAliases()
+               .Build();
+        
+        private static readonly IDeserializer _deserializer =
+            new DeserializerBuilder()
+               .WithNamingConvention(PascalCaseNamingConvention.Instance)
+               .WithTypeResolver(new DynamicTypeResolver())
+               .Build();
         
         protected override string Extension => "yaml";
         

4c59b7dfc9fc071db1fd97de5d9b1cc02c0a9e00
Nikita Grishin <nikitagrishin1993@yandex.ru>
2019-12-03 22:36:26 +0300

    #17

---
+++ Basics.Test/StreamExtensionsTest.cs
@@ -0,0 +1,36 @@
+namespace SpaceEngineers.Core.Basics.Test
+{
+    using System.IO;
+    using System.Text;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class StreamExtensionsTest : BasicsTestBase
+    {
+        public StreamExtensionsTest(ITestOutputHelper output)
+            : base(output) { }
+
+        [Fact]
+        internal void OwerWriteAllTest()
+        {
+            const string @long = "Hello world!";
+            const string @short = "Hello!";
+         
+            var encoding = new UTF8Encoding();
+            
+            var longBytes = encoding.GetBytes(@long);
+            var shortBytes = encoding.GetBytes(@short);
+
+            using (var stream = new MemoryStream(longBytes))
+            {
+                var readed = stream.ReadAllAsync(encoding).Result;
+                Assert.Equal(@long, readed);
+
+                stream.OverWriteAllAsync(shortBytes).Wait();
+                
+                readed = stream.ReadAllAsync(encoding).Result;
+                Assert.Equal(@short, readed);
+            }
+        }
+    }
+}

94d981522740d515fbb70777d870027178bfba06
nikitagrishin <nikitagrishin1993@yandex.ru>
2022-03-26 14:22:45 +0300

    #168

--- Tests/Roslyn.Test/Implementations/CodeFixVerifier.cs
+++ Tests/Roslyn.Test/Implementations/CodeFixVerifier.cs
@@ -20,13 +20,6 @@ namespace SpaceEngineers.Core.Roslyn.Test.Implementations
     [Component(EnLifestyle.Singleton)]
     internal class CodeFixVerifier : ICodeFixVerifier
     {
-        private readonly NewLineSourceTransformer _newLineSourceTransformer;
-
-        public CodeFixVerifier(NewLineSourceTransformer newLineSourceTransformer)
-        {
-            _newLineSourceTransformer = newLineSourceTransformer;
-        }
-
         public async Task VerifyCodeFix(DiagnosticAnalyzer analyzer,
                                         CodeFixProvider codeFix,
                                         AnalyzedDocument analyzedDocument,
@@ -34,7 +27,7 @@ namespace SpaceEngineers.Core.Roslyn.Test.Implementations
                                         Action<string> show)
         {
             var fixedDocument = await ApplyFix(codeFix, analyzedDocument.Document, analyzedDocument.ActualDiagnostics).ConfigureAwait(false);
-            var actualSource = _newLineSourceTransformer.Transform(await fixedDocument.GetTextAsync().ConfigureAwait(false));
+            var actualSource = await fixedDocument.GetTextAsync().ConfigureAwait(false);
 
             if (expectedSource.Text.ToString() != actualSource.ToString())
             {
